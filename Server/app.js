dec29783375e48ad5ddf5ddc7c9d628b3e643fa7
davidkenesaw <davidkennesaw@gmail.com>
2023-09-13 19:18:24 -0400

    initial commit

--- Server/app.js
+++ Server/app.js
@@ -6,6 +6,16 @@ app.get("/",function(req,res){
     res.send("Hello World")
 })
 
+app.get("/About", function(req,res){
+    res.send("About page")
+})
+app.get("/Home", function(req,res){
+    res.send("Home page")
+})
+app.get("/Login", function(req,res){
+    res.send("Login page")
+})
+
 app.listen("3000", function(){
     console.log("listening on port 3000")
 })
