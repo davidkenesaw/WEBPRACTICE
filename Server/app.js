9e2f641504bb7d0d2e6af1035ddb04faa3854591
davidkenesaw <davidkennesaw@gmail.com>
2023-09-13 19:50:29 -0400

    initial commit

--- Server/app.js
+++ Server/app.js
@@ -1,19 +1,26 @@
 const express = require('express');
-
+const path = require('path')
 const app = express();
+
+
+app.set('view engine', 'ejs');//use ejs
+app.use(express.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, '../Client/views'));//show express the views directory
+
+
 
 app.get("/",function(req,res){
     res.send("Hello World")
 })
 
 app.get("/About", function(req,res){
-    res.send("About page")
+    res.render("About")
 })
 app.get("/Home", function(req,res){
-    res.send("Home page")
+    res.render("Home")
 })
 app.get("/Login", function(req,res){
-    res.send("Login page")
+    res.render("Login")
 })
 
 app.listen("3000", function(){
