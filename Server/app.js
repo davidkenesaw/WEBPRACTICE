747e801d32545b4d19dd4ca650ccd8dc56855de0
davidkenesaw <davidkennesaw@gmail.com>
2023-09-13 19:56:47 -0400

    comment

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 
 const app = express();
-
+//root route
 app.get("/",function(req,res){
+    //sends hello world to the browser
     res.send("Hello World")
 })
 
